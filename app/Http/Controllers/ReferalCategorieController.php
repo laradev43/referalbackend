eb1ab18c52295a483c947e4a5993a20e24a7c230
laradev43 <contact@laradev.fr>
2021-04-23 21:25:53 +0200

    Referal cateogry & migration MCR

---
+++ app/Http/Controllers/ReferalCategorieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ReferalCategorie;
+use Illuminate\Http\Request;
+
+class ReferalCategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ReferalCategorie  $referalCategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReferalCategorie $referalCategorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ReferalCategorie  $referalCategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReferalCategorie $referalCategorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ReferalCategorie  $referalCategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReferalCategorie $referalCategorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ReferalCategorie  $referalCategorie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReferalCategorie $referalCategorie)
+    {
+        //
+    }
+}
